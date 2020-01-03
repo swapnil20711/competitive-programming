ec16f639e7b7425281ec5a77c521d41d76c5d028
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-01-03 21:45:45 +0530

    Playing With Characters

---
+++ play.c
@@ -0,0 +1,15 @@
+//Hackerrank playing with characters
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char ch,s[100],sen[200];
+    scanf("%c",&ch);
+    scanf("%s",s);
+    getchar();//If someone hits enter to get it getchar is used
+    gets(sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    puts(sen);
+
+}
