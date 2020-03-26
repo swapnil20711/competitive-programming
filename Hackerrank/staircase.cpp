aceeef463b531624070f58c10725c6c4c701bf9d
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-03-26 13:20:09 +0530

    Staircase  hackerrank

---
+++ Hackerrank/staircase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, l, i, j;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (l = 0; l < n - i - 1; l++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "#";
+        }
+        cout << endl;
+    }
+    return 0;
+}
