42524ace7a5190f117cb32c8de8db7f72c8c20e7
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-01 09:44:33 +0530

    SPOJ SMPWOW - Wow

---
+++ Spoj/wow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << "W";
+    for (int i = 0; i < x; i++)
+    {
+        cout << "o";
+    }
+    cout << "w";
+    return 0;
+}
